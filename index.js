98109f4da38b757596716cdb82198abb9881863d
Anurag <anuragawasthi020@gmail.com>
2022-03-31 14:24:07 +0530

    commmit

--- index.js
+++ index.js
@@ -1,7 +1,80 @@
-const fs = require('fs');
-const util = require('util');
+var fs = require('fs');
+const fs = fsWithCallbacks.promises;
 
-util.promisify(fs.readFile)('README.md').then((hash) => {
-    console.log(hash.toString().trim());
-});
+const getGitId = async () => {
+  const gitId = await fs.readFile('.git/HEAD', 'utf8');
+  if (gitId.indexOf(':') === -1) {
+    return gitId;
+  }
+  const refPath = '.git/' + gitId.substring(5).trim();
+  return await fs.readFile(refPath, 'utf8');
+};
+
+const gitId = await getGitId();
+console.log(gitId)
 console.log('here');
+
+
+// var Git = require( 'nodegit' );
+
+// Git.Repository.open('https://github.com/amdollar/GitContextTesting.git').then( function( repository ) {
+//   return repository.getHeadCommit( );
+// } ).then( function ( commit ) {
+//   return commit.sha();
+// } ).then( function ( hash ) {
+//   // use `hash` here
+// } );
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const util = require('util');
+
+// util.promisify(fs.readFile)('README.md').then((hash) => {
+//     console.log(hash.toString().trim());
+// });
+// console.log('here');
+
+// var sys = require('sys');
+// var Git = require('git');
+// // Git("/Users/tim/git/howtonode.org.git");
+// Git("/Users/tim/Code/howtonode.org");
+// Git.log("articles/what-is-this.markdown", function (err, data) {
+//   if (err) throw err;
+//   sys.p(data);
+//   process.exit();
+// });
+
+// var sys = require('sys'),
+//     Git = require('git');
+
+
+// // Test it!
+// Git("/Users/tim/code/howtonode.org");
+// Git.exists("articles/control-flow-part-ii.markdown", function (err, tags) {
+//   if (err) { throw(err); }
+//   sys.p(tags);
+// });
+// Git.getTags(function (err, tags) {
+//   if (err) { throw(err); }
+//   Object.keys(tags).forEach(function (tag) {
+//     Git.readDir("articles", tags[tag], function (err, contents) {
+//       if (err) { throw(err); }
+//       contents.files.forEach(function (file) {
+//         file = Path.join("articles", file);
+//         Git.readFile(file, tags[tag], function (err, text) {
+//           if (err) { throw(err); }
+//           sys.error("tag: " + tag + " sha1: " + tags[tag] + " file: " + file + " length: " + text.length);
+//         });
+//       });
+//     });
+//   });
+// });
