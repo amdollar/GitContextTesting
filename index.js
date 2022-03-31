29ada3fd585f1c60804c5730b2a7a6530330e000
Anurag <anuragawasthi020@gmail.com>
2022-03-31 13:48:10 +0530

    Adding more data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const util = require('util');
 
-util.promisify(fs.readFile)('.git/refs/heads/main').then((hash) => {
+util.promisify(fs.readFile)('.git/refs/heads/master').then((hash) => {
     console.log(hash.toString().trim());
 });
 console.log('here');
